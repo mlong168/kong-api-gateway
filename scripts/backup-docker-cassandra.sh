9eb9c4fcfd54b60b377969f90d5f9f5a87d626c2
Matthew Long <mlong@rozoom.com>
2016-04-19 14:28:31 -0500

    some more backup script cleanup

--- scripts/backup-docker-cassandra.sh
+++ scripts/backup-docker-cassandra.sh
@@ -12,15 +12,14 @@ ks="kong"
 bucket="s3://kong-db-backup"
 datadir="/var/lib/cassandra/data/${ks}"
 name=$(date '+%m%d%y-%H:%M')
-tarball_file="/backup/${ks}-${name}.tar.gz"
-tarball_name="${ks}-${name}.tar.gz"
+tarball_file="${ks}-${name}.tar.gz"
 ip=$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' cassandra)
 
 docker exec cassandra nodetool flush $ks
 docker exec cassandra nodetool snapshot $ks -t $name
-docker run --rm --volumes-from cassandra-data -v $(pwd):/backup mini/base sh -c "tar -czf $tarball_file $datadir/*/snapshots/$name"
+docker run --rm --volumes-from cassandra-data -v $(pwd):/backup mini/base sh -c "tar -czf /backup/$tarball_file $datadir/*/snapshots/$name"
 
 # assume /var/lib/cassandra is the volume in the container (for now)
 if [ $? -eq 0 ] ; then
-        aws s3 cp $tarball_name "${bucket}/${tarball_name}"
+        aws s3 cp $tarball_file "${bucket}/${tarball_file}"
 fi
