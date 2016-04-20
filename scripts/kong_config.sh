50b82389bc208da3d9a50bb0533417a6e29323b0
Matthew Long <mlong@rozoom.com>
2016-04-19 23:19:30 -0500

    added vars to config script

--- scripts/kong_config.sh
+++ scripts/kong_config.sh
@@ -3,18 +3,22 @@
 # Change this to 127.0.0.1 if running on linux
 
 host="127.0.0.1"
+request_host="staging-s-sprint.pizzahut.com"
+upstream_url="http://services.midwest01.quikorder.com"
+username="pizzahut"
+redirect_uri="https://pizzahut.com/"
 
 # Creating the API:
-curl -d "request_host=staging-s-sprint.pizzahut.com&upstream_url=http://services.midwest01.quikorder.com" $host:8001/apis
+curl -d "request_host=$request_host&upstream_url=$upstream_url" $host:8001/apis
 
 # Adding the OAuth 2.0 Plugin:
 
-curl -d "name=oauth2&config.scopes=profile,email&config.enable_authorization_code=false&config.enable_password_grant=true" $host:8001/apis/staging-s-sprint.pizzahut.com/plugins/
+curl -d "name=oauth2&config.scopes=profile,email&config.enable_authorization_code=false&config.enable_password_grant=true" $host:8001/apis/$request_host/plugins/
 
 # Creating a Consumer:
 
-curl -d "username=pizzahut" $host:8001/consumers/
+curl -d "username=$username" $host:8001/consumers/
 
 # Creating OAuth 2.0 credentials for the consumer:
 
-curl -d "name=PHI-Staging&redirect_uri=https://pizzahut.com/" $host:8001/consumers/pizzahut/oauth2/
+curl -d "name=PHI-Staging&redirect_uri=$redirect_uri" $host:8001/consumers/pizzahut/oauth2/
