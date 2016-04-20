c33e4451508373520209182a04cf966df9fca70a
Matthew Long <mlong@rozoom.com>
2016-04-19 23:09:56 -0500

    kong config for oauth2

---
+++ scripts/kong_config.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Change this to 127.0.0.1 if running on linux
+
+host="192.168.99.100"
+
+# Creating the API:
+curl -d "request_host=staging-s-sprint.pizzahut.com&upstream_url=http://services.midwest01.quikorder.com" $host:8001/apis
+
+# Adding the OAuth 2.0 Plugin:
+
+curl -d "name=oauth2&config.scopes=profile,email&config.enable_authorization_code=false&config.enable_password_grant=true" $host:8001/apis/staging-s-sprint.pizzahut.com/plugins/
+
+# Creating a Consumer:
+
+curl -d "username=pizzahut" $host:8001/consumers/
+
+# Creating OAuth 2.0 credentials for the consumer:
+
+curl -d "name=PHI-Staging&redirect_uri=https://pizzahut.com/" $host:8001/consumers/pizzahut/oauth2/
---
+++ scripts/kong_test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+username="pizzahut"
+password="pizzahut"
+client_id="c652b1e804a941b9bddec72b88c019da"
+client_secret="6d62c7e94f0f4ded938189f556201720"
+provision_key="23516763ec0c4311c39c48c34c5a881d"
+authenticated_userid="phi"
+access_token=""
+host="staging-s-sprint.pizzahut.com"
+docker_host="192.168.99.100"
+
+# Requesting the access token (setting the authenticated user ID to bob):
+
+curl -d "grant_type=password&username=$username&password=$password&scope=email&client_id=$client_id&client_secret=$client_secret&provision_key=$provision_key&authenticated_userid=$authenticated_userid" -H "Host: $host" https://$docker_host:8443/oauth2/token --insecure
+
+# Consuming the API with the access token:
+
+#curl -H "Host: $host" https://$docker_host:8443/get?access_token=$access_token --insecure
