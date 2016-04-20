bfb7c299eae44c7bf3b752dc1b1bee137501d795
Matthew Long <mlong@rozoom.com>
2016-04-20 13:36:54 -0500

    updated comments to kong_test

--- scripts/kong_test.sh
+++ scripts/kong_test.sh
@@ -10,10 +10,10 @@ access_token=""
 host="staging-s-sprint.pizzahut.com"
 docker_host="127.0.0.1"
 
-# Requesting the access token (setting the authenticated user ID to bob):
+# Requesting the access token (setting the authenticated user ID to pizzahut):
 
 curl -d "grant_type=password&username=$username&password=$password&scope=email&client_id=$client_id&client_secret=$client_secret&provision_key=$provision_key&authenticated_userid=$authenticated_userid" -H "Host: $host" https://$docker_host:8443/oauth2/token --insecure
 
-# Consuming the API with the access token:
+# Consuming the API with the access token. Need to get access_token from output of command above and insert into access_token var. Then uncomment and run.
 
 #curl -H "Host: $host" https://$docker_host:8443/get?access_token=$access_token --insecure
